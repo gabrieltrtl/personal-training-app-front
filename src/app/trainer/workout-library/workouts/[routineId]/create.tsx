21ec2b59582c34e33d3b6a81cb503aaa70e6fdb5
Gabriel Lira <gabriel.tertulino@proton.me>
2025-07-03 14:58:42 -0300

    1.4.1

--- src/app/trainer/workout-library/workouts/[routineId]/create.tsx
+++ src/app/trainer/workout-library/workouts/[routineId]/create.tsx
@@ -42,7 +42,7 @@ export default function CreateWorkout() {
       const workoutId = response.data.id;
 
       console.log("Workout ID gerado:", workoutId);
-      router.push(`/trainer/workout-library/exercises/${workoutId}/create`);
+      router.push(`/trainer/workout-library/workouts/${workoutId}`);
     } catch (error: any) {
       console.error(
         "Erro ao criar treino:",
